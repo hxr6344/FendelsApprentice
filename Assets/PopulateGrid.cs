b7116a603c186b2a515e093d097ba75bc4f1263d
Danny Koester <dkoester306@gmail.com>
2018-10-08 00:52:12 -0400

    Scrollable list for elements added.

---
+++ Assets/PopulateGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PopulateGrid : MonoBehaviour {
+
+	public GameObject item;
+
+	public Sprite[] sprites;
+
+	int counter;
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0;i<sprites.Length;i++)
+		{
+			GameObject temp= (GameObject)Instantiate(item,transform);
+			temp.GetComponent<Image>().sprite = sprites[i];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+}
