2f4cd149a3efc22688fcceb2273c369fef4b8671
dkoester306 <dkoester306@gmail.com>
2018-10-14 21:41:23 -0400

    file structure work in progress.

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Element {
+
+	public string elementName;
+	public string iconName;	// the name of the png file for this element.
+	public int elementID;	// id of the element. if it is 0, then the element is a base element.
+	public int ingred1;		// the elementID of the first ingredient;
+	public int ingred2;		// the elementID of the second ingredient;
+}
---
+++ Assets/Scripts/ElementLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ElementLoader : MonoBehaviour {
+
+	public List<Element> elementsList;
+	public Dictionary<Vector2Int,Element> elements;
+
+	// Use this for initialization
+	void Awake () {
+		elementsList = new List<Element>();
+		ElementDictionary dictionary = JsonUtility.FromJson<ElementDictionary>(JsonFileReader.LoadJsonAsResource("Elements/ElementDictionary.json"));
+		foreach(string dictionaryItem in dictionary.elements)
+		{
+			LoadItem(dictionaryItem);
+		}
+
+		foreach(KeyValuePair<Vector2Int,Element> entry in elements)
+		{
+			Element temp = elements[entry.Key];
+		}
+	}
+
+	public void LoadItem(string path)
+	{
+		string myLoadedItem = JsonFileReader.LoadJsonAsResource(path);
+		Element myElement = JsonUtility.FromJson<Element>(myLoadedItem);
+		elementsD.Add(new Vector2Int(myElement.ingred1,myElement.ingred2),myElement);
+	}
+	
+
+}
