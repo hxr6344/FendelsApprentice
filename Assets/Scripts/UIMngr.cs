ae781603e570d689b99453e06e6aa40a5f8fc6bb
Katy Mollenkopf <kmm5278@rit.edu>
2018-10-16 14:23:55 -0400

    Added options menu with some functionality. Added HUD

---
+++ Assets/Scripts/UIMngr.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+///<summary> Handles all the toggling of menus </summary>
+public class UIMngr : MonoBehaviour
+{
+
+    #region Singleton Instance Checks
+    //Ensure there is only one instance of the UI manager
+    private static UIMngr instance = null;
+    public static UIMngr Instance
+    {
+        get
+        {
+            //Find the UI manager if it exists
+            if(instance == null)
+            {
+                instance = FindObjectOfType<UIMngr>();
+            }
+            //Create a new one if it doesn't
+            if(instance == null)
+            {
+                GameObject obj = new GameObject("UIMngr");
+                instance = obj.AddComponent<UIMngr>();
+            }
+
+            return instance;
+        }
+    }
+
+    //Ensure the instance is removed when the game is closed
+    void OnApplicationQuit()
+    {
+        instance = null;
+    }
+    #endregion
+
+    public GameObject hud;
+    public GameObject options;
+    public GameObject overlay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Toggle the HUD
+        hud.SetActive(true);
+	}
+
+    //Toggles the options menu
+    public void ToggleOptions()
+    {
+        if (hud.active)
+        {
+            Debug.Log("options enabled");
+            hud.SetActive(false);
+            options.SetActive(true);
+            overlay.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("options disabled");
+            hud.SetActive(true);
+            options.SetActive(false);
+            overlay.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
